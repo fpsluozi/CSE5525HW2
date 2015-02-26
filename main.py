8171aa1832c5870b8374dd40e02c23411589a635
FPSLuozi <rosk-07@163.com>
2015-02-25 19:32:31 -0500

    Adding Step 1 header
    Step 1 header

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # CSE5525 NLP Homework 2 Group 1
-Test
+import nltk
+from nltk.corpus import treebank
+
+full_training_set = nltk.corpus.treebank.tagged_sentences()[0:3500]
+training_set1 = full_training_set[0:1750]
+training_set2 = full_training_set[1750:]
+test_set = nltk.corpus.treebank.tagged_sentences()[3500:]
