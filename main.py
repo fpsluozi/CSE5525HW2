ff0d2ba8d0b6c059b6bcfae7ed0a6d139f9850f7
FPSLuozi <rosk-07@163.com>
2015-02-25 21:31:44 -0500

    Adding start end tags

--- main.py
+++ main.py
@@ -19,12 +19,14 @@ test_set = nltk.corpus.treebank.tagged_sents()[3500:]
 # 	meaning print the prob of word 'the' given determinor from full training set
 #
 # PS. cpd as the Conditional Prob Distribution
-# PSS. We use Laplace dsistribution for unseen cases
+# PSS. We use Laplace distribution for unseen cases
 
 # Full Traning Set
 full_training_set_words = []
 for sent in full_training_set:
+	full_training_set_words.append(('S','S'))
 	full_training_set_words.extend([ (tag, word) for (word, tag) in sent ])
+	full_training_set_words.append(('T','T'))
 
 full_cfd_word_tag = nltk.ConditionalFreqDist(full_training_set_words)
 full_cpd_word_tag = nltk.ConditionalProbDist(full_cfd_word_tag, nltk.LaplaceProbDist)
@@ -36,7 +38,9 @@ full_cpd_tags = nltk.ConditionalProbDist(full_cfd_tags, nltk.LaplaceProbDist)
 #Traning Set 1
 set1_training_set_words = []
 for sent in training_set1:
+	set1_training_set_words.append(('S','S'))
 	set1_training_set_words.extend([ (tag, word) for (word, tag) in sent ])
+	set1_training_set_words.append(('T','T'))
 
 set1_cfd_word_tag = nltk.ConditionalFreqDist(set1_training_set_words)
 set1_cpd_word_tag = nltk.ConditionalProbDist(set1_cfd_word_tag, nltk.LaplaceProbDist)
