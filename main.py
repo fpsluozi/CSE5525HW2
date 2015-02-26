7e26f2b1c6278275e9188b40b88782297af22f48
wangdingkang <1257332448@qq.com>
2015-02-25 19:48:17 -0500

    initialize function

--- main.py
+++ main.py
@@ -8,3 +8,23 @@ training_set2 = full_training_set[1750:]
 test_set = nltk.corpus.treebank.tagged_sents()[3500:]
 
 print full_training_set
+
+dict_words = {}
+dict_tags = {}
+num_words = 0
+num_tags = 0
+
+for sent in nltk.corpus.treebank.tagged_sents():
+    for word_tag in sent:
+        word = word_tag[0]
+        tag = word_tag[1]
+        if dict_words.get(word) == None:
+            dict_words[word] = num_words
+            num_words = num_words + 1
+        if dict_tags.get(tag) == None:
+            dict_tags[tag] = num_tags
+            num_tags = num_tags + 1
+
+import numpy
+t_w_table = numpy.zeros((num_tags, num_words))
+t_t_table = numpy.zeros((num_tags, num_tags))
