4c007ef91962976e3bc66967ac808986e2d3f2ee
Martin Lo <kuanwenlo@gmail.com>
2015-03-02 10:53:56 -0500

    update viterbi

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import nltk
 import numpy
 import itertools
+from viterbi import viterbi
 from nltk.corpus import treebank
 
 
@@ -89,64 +90,17 @@ set1_cpd_tags = nltk.ConditionalProbDist(set1_cfd_tags, nltk.MLEProbDist)
 # Since both emission prob and transition prob can be zero, we use an extremely small epsilon
 # to rule out zero probs during log calculation.
 
-def viterbi(obs, states, start_p, trans_p, emit_p):
-    V = [{}]
-    path = ['']
-    epsilon = 0.00000000000000000001
- 
-    # Initialize base cases (t == 0)
-    # Actually it will already be <s>
 
-    max_v = -1.0 / epsilon / 1000
-    for y in states:
-        temp_emit = emit_p[y].prob(obs[0])
-        if temp_emit < epsilon:
-            temp_emit = epsilon
-        V[0][y] = math.log(start_p[y]) + math.log(temp_emit)
-        if max_v < V[0][y]: 
-            max_v = V[0][y]          
-            path[0] = y
- 
-    # Run Viterbi for t > 0
-    for t in range(1, len(obs)):
-         
-        V.append({})
-        path.append('')
-        max_v = -1.0 / epsilon
-        for y in states:
-            
-            if y == "<s>":
-                V[t][y] = max_v
-            else:
-                temp = list()
-                for y0 in states: 
-                    temp_trans = trans_p[y0].prob(y)
-                    if temp_trans < epsilon:
-                        temp_trans = epsilon
-                    temp_prev_v = V[t-1][y0]
-                    if temp_prev_v < epsilon:
-                        temp_prev_v = epsilon
-                    temp.append((math.log(temp_prev_v) + math.log(temp_trans)))
 
-                temp_emit = emit_p[y].prob(obs[t])
-                if temp_emit < epsilon:
-                    temp_emit = epsilon
-                prob = math.log(temp_emit) + max(temp)
-                V[t][y] = prob
-                if max_v < prob:              
-                    max_v = prob
-                    path[t] = y
-
-    # Outputting the Viterbi path    
-    return path
-
-# The init_table enforces every sentence to start with <s>
+# The init_table is the initial probabilities and enforces every sentence to start with <s>
 init_table = {}
 for tag in full_tag_set:
     init_table[tag] = 0.00000000000000000001
 init_table['<s>'] = 1.0
 
+# test_obs is a sentence to test viterbi
 test_obs = ['Pierre', 'Viken', ',' , '61' , "years", "old", "will", "join", "the", "board", "as", "a", "nonexecutive", "director", "Nov.", "29","."]
+#run viterbi
 print viterbi(test_obs, full_tags, init_table, full_cpd_tags, full_cpd_word_tag )
 
 # Step 4
--- viterbi.py
+++ viterbi.py
@@ -1,27 +1,52 @@
-import numpy
-class Decoder(object):
-    #create object with initial prob, trans prob and emission prob
-    def __init__(self, initialProb, transProb, obsProb):
-        self.N = initialProb.shape[0]
-        self.initialProb = initialProb
-        self.transProb = transProb
-        self.obsProb = obsProb
-        assert self.initialProb.shape == (self.N, 1)
-        assert self.transProb.shape == (self.N, self.N)
-        assert self.obsProb.shape[0] == self.N
+import math
+
+def viterbi(obs, states, start_p, trans_p, emit_p):
+    V = [{}]
+    path = ['']
+    epsilon = 0.00000000000000000001
  
-    def Obs(self, obs):
-        return self.obsProb[:, obs, None]
-    def Viterbi(self, obs):
-        #initialization
-        viterbi = numpy.zeros((self.N, len(obs)))
-        backpt = numpy.ones((self.N, len(obs)), 'int32') * -1
-        viterbi[:, 0] = numpy.squeeze(self.initialProb * self.Obs(obs[0]))
-        #recursion
-        for t in xrange(1, len(obs)):
-            viterbi[:, t] = (viterbi[:, t-1, None].dot(self.Obs(obs[t]).T) * self.transProb).max(0)
-            backpt[:, t] = (numpy.tile(viterbi[:, t-1, None], [1, self.N]) * self.transProb).argmax(0)
-        tokens = [viterbi[:, -1].argmax()]
-        for i in xrange(len(obs)-1, 0, -1):
-        tokens.append(backpt[tokens[-1], i])
-        return tokens[::-1]
+    # Initialize base cases (t == 0)
+    # Actually it will already be <s>
+
+    max_v = -1.0 / epsilon / 1000
+    for y in states:
+        temp_emit = emit_p[y].prob(obs[0])
+        if temp_emit < epsilon:
+            temp_emit = epsilon
+        V[0][y] = math.log(start_p[y]) + math.log(temp_emit)
+        if max_v < V[0][y]: 
+            max_v = V[0][y]          
+            path[0] = y
+ 
+    # Run Viterbi for t > 0
+    for t in range(1, len(obs)):
+         
+        V.append({})
+        path.append('')
+        max_v = -1.0 / epsilon
+        for y in states:
+            
+            if y == "<s>":
+                V[t][y] = max_v
+            else:
+                temp = list()
+                for y0 in states: 
+                    temp_trans = trans_p[y0].prob(y)
+                    if temp_trans < epsilon:
+                        temp_trans = epsilon
+                    temp_prev_v = V[t-1][y0]
+                    if temp_prev_v < epsilon:
+                        temp_prev_v = epsilon
+                    temp.append((math.log(temp_prev_v) + math.log(temp_trans)))
+
+                temp_emit = emit_p[y].prob(obs[t])
+                if temp_emit < epsilon:
+                    temp_emit = epsilon
+                prob = math.log(temp_emit) + max(temp)
+                V[t][y] = prob
+                if max_v < prob:              
+                    max_v = prob
+                    path[t] = y
+
+    # Outputting the Viterbi path    
+    return path
